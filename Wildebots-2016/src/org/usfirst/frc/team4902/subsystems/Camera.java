b7f52650e286d100c262ccd15706ca770f64ac29
George Lewis <bolt2strike@me.com>
2016-02-02 14:22:32 -0500

    Commit branch

--- Wildebots-2016/src/org/usfirst/frc/team4902/subsystems/Camera.java
+++ Wildebots-2016/src/org/usfirst/frc/team4902/subsystems/Camera.java
@@ -1,11 +1,14 @@
 package org.usfirst.frc.team4902.subsystems;
 
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 // This class handles the Camera subsystem and provides services related to the camera server
 public class Camera {
 	
 	private static CameraServer camera;
+	
+	private static NetworkTable contours = NetworkTable.getTable("GRIP/Contours");
 	
 	// This function starts the camera server and can be seen in the FRC Driver Station
 	public static void startCamera() {
@@ -14,4 +17,7 @@ public class Camera {
         // The camera name (eg "cam0") can be found through the Roborio web interface
 		camera.startAutomaticCapture("cam0");
 	}
+	
+	public double
+	
 }
