26520e84b7c69cc5627c0697ee3a24fe65dda8bd
George Lewis <bolt2strike@me.com>
2016-02-06 10:34:40 -0500

    Fixed a messed up merge

--- Wildebots-2016/src/org/usfirst/frc/team4902/subsystems/Gyrometer.java
+++ Wildebots-2016/src/org/usfirst/frc/team4902/subsystems/Gyrometer.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team4902.subsystems;
 
-import org.usfirst.frc.team4902.robot.PortMap;
-
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 
 
@@ -9,31 +7,19 @@ public class Gyrometer {
 
 	private static Gyrometer instance = new Gyrometer();
 
-	private Gyro gyro = new AnalogGyro(PortMap.Gyro.getPort());
-	
-<<<<<<< HEAD
+	private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
+
 	public static Gyrometer getInstance() {
 		return instance;
-=======
-	private ADXRS450_Gyro gyro;
-	
-	public Gyrometer() {
-		gyro = new ADXRS450_Gyro();
-		gyro.reset();
->>>>>>> refs/remotes/origin/Gyro
 	}
+
 
 	// Returns angle between 0 and 360 degrees relative to calibrated angle
 	public double getAngle() {
 		return -gyro.getAngle();
 	}
-<<<<<<< HEAD
 
 	public void reset() {
-=======
-	
-	public void calibrate() {
->>>>>>> refs/remotes/origin/Gyro
 		gyro.reset();
 	}
 
