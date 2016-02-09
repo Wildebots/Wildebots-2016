2182e1ef206c4e8c255e1b97c0cb2a8c730bc089
Gunvir Ranu <g1ranu@gmail.com>
2016-02-09 17:43:39 -0500

    Updated shooter system PID control. Removed Alex's code. Added code to control shooter motor using encoder.

--- Wildebots-2016/src/org/usfirst/frc/team4902/subsystems/ShooterSystem.java
+++ Wildebots-2016/src/org/usfirst/frc/team4902/subsystems/ShooterSystem.java
@@ -10,68 +10,18 @@ import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Talon;
 
-public class ShooterSystem extends Subsystem{
-	
-	/**
-	 * This class converts the encoder's value into a form the PIDController can take as an input
-	 *
-	 *
-	 */
-	private class EncoderPIDSource implements PIDSource{
-		
-		@Override
-		public void setPIDSourceType(PIDSourceType pidSource) {
-		}
-		@Override
-		public PIDSourceType getPIDSourceType() {
-			return null;
-		}
-		@Override
-		public double pidGet() {
-			return Encoders.getInstance().getArmMotorEncoder(); //TODO: this is probably not the right input...
-		}
-	}
-	
-	/**
-	 * 
-	 * This class will take the values processed by the PIDController and interpret it as an output in motor movement
-	 *
-	 */
-	private class ArmPIDOutput implements PIDOutput{
-		@Override
-		public void pidWrite(double output) {
-			armMotor.set(output);
-		}
-	}
-
+public class ShooterSystem {
 	
 	private static ShooterSystem instance = new ShooterSystem();
 	
 	private int temp = 0; // TODO: add entries in PortMap
-
 	
 	private Talon leftShooterMotor = new Talon(temp), rightShooterMotor = new Talon(temp), armMotor = new Talon(temp);
 	
-	private EncoderPIDSource encoderPIDSource = new EncoderPIDSource();
-	private ArmPIDOutput armPIDOutput = new ArmPIDOutput();
-	private PIDController shooterArm = new PIDController(1,1,1,encoderPIDSource,armPIDOutput); // TODO: Get actual PID values
+	private PIDController shooterArm = new PIDController(0.05, 0.0, 0.5, Encoders.getInstance().getShooterEncoder(), armMotor); // TODO: Get actual PID values
 	
 	public static ShooterSystem getInstance(){
 		return instance;
-	}
-	
-	
-	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
-		
-	}
-	
-
-	@Override
-	public void log() {
-		// TODO Auto-generated method stub
-		
 	}
 	
 	/**
@@ -81,6 +31,7 @@ public class ShooterSystem extends Subsystem{
 		leftShooterMotor.set(1);
 		rightShooterMotor.set(1);
 	}
+	
 	/**
 	 * sets the angle of the arm to a specified value
 	 * @param angle The desired angle to set the arm to
