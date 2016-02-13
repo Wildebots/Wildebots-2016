a082e35eb7b74168078f4a5132b1b89c68fb31df
Gunvir Ranu <g1ranu@gmail.com>
2016-02-13 12:33:40 -0500

    To fix merge conflict.

---
+++ Wildebots-2016/src/org/usfirst/frc/team4902/robot/Robot.java
@@ -0,0 +1,31 @@
+
+package org.usfirst.frc.team4902.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.DigitalOutput;
+import edu.wpi.first.wpilibj.util.AllocationException;
+
+public class Robot extends IterativeRobot{
+		
+    public void robotInit(){
+    }
+    
+    public void autonomousInit() {
+    	
+    }
+
+    public void autonomousPeriodic() {
+    	
+    }
+
+    public void teleopPeriodic(){
+    }
+       
+    public void testPeriodic() {
+    	
+    }
+    
+}
