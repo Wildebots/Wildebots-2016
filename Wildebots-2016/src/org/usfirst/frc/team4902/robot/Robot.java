30236d44edb052b60ba1bdd4228719eb386ea0e9
George Lewis <bolt2strike@me.com>
2016-02-16 16:00:17 -0500

    Initial branch commit

--- Wildebots-2016/src/org/usfirst/frc/team4902/robot/Robot.java
+++ Wildebots-2016/src/org/usfirst/frc/team4902/robot/Robot.java
@@ -20,7 +20,7 @@ import edu.wpi.first.wpilibj.tables.ITable;
  * Starting point of program
  */
 public class Robot extends IterativeRobot {
-		
+	
 	private static Robot instance;
 	
 	public static Robot getInstance() {
